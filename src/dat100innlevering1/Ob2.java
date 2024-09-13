141509645054e2fd8b95b92ac8f5daf9305115d0
798118 <164620581+TURBOTHAI@users.noreply.github.com>
2024-09-13 22:21:25 +0200

    fikset misforståelse

--- src/dat100innlevering1/Ob2.java
+++ src/dat100innlevering1/Ob2.java
@@ -8,7 +8,12 @@ public class Ob2 {
 	    public static void main(String[] args) {
 	        for (int i=1; i<=10; i++) {
 	            int Bruttoinntekt;
-
+	            double t1=0.0;
+	            double t2=0.0;
+	            double t3=0.0;
+	            double t4=0.0;
+	            double t5=0.0;
+	           
 	            while (true) {
 	            	Bruttoinntekt = parseInt(showInputDialog("skriv inn Bruttoinntekten: "));
 	            	
@@ -17,19 +22,25 @@ public class Ob2 {
 	            	} else {
 	            		 System.out.println("Error, skriv inn på nytt");
 	            	 }
+	     
 	            }
 	            if (Bruttoinntekt <= 208050) {
 	                System.out.println(Bruttoinntekt + " KR - Ingen trinnskatt");
 	            } else if (Bruttoinntekt <= 292850) {
-	                System.out.println((Bruttoinntekt * 0.017) + " KR - Trinn 1 skatt");
+	            	t1=(292850-208051)*0.017; 
+	                System.out.println((t1) + " KR - Trinn 1 skatt");
 	            } else if (Bruttoinntekt <= 670000) {
-	                System.out.println((Bruttoinntekt * 0.04) + " KR - Trinn 2 skatt");
+	            	t2=(670000-292851)*0.04; 
+	                System.out.println((t2+t1) + " KR - Trinn 2 skatt");
 	            } else if (Bruttoinntekt <= 937900) {
-	                System.out.println((Bruttoinntekt * 0.136) + " KR - Trinn 3 skatt");
-	            } else if (Bruttoinntekt <= 1999999) {  // Justert øvre grense
-	                System.out.println((Bruttoinntekt * 0.176) + " KR - Trinn 4 skatt");
+	            	t3=(937900-670001)*0.136;
+	                System.out.println((t3+t2+t1) + " KR - Trinn 3 skatt");
+	            } else if (Bruttoinntekt <= 937901) {
+	            	t4=(1350000-937901)*0.166;// Justert øvre grense
+	                System.out.println((t4+t3+t2+t1) + " KR - Trinn 4 skatt");
 	            } else {
-	                System.out.println((Bruttoinntekt * 0.178) + " KR - Trinn 5 skatt");
+	            	t5=(1350001-Bruttoinntekt)*0.176;
+	                System.out.println((t5+t4+t3+t2+t1) + " KR - Trinn 5 skatt");
 	            }
 	        }
 	    }
